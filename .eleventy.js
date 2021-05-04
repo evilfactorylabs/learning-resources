87e1062cc84c46e80233e7fcb73b6a77b2aa9dec
fariz <fariz@evilfactory.id>
2021-05-05 01:48:31 +0700

    feat: configure static generator

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = eleventyConfig => {
+  const markdownItOpts = {
+    html: true,
+    breaks: true,
+    linkify: true
+  }
+
+  const markdownIt = require('markdown-it')
+  const markdownItCheckbox = require('markdown-it-checkbox')
+
+  const markdownLib = markdownIt(markdownItOpts).use(markdownItCheckbox)
+
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.setLibrary('md', markdownLib)
+
+  return {
+    passthroughFileCopy: true
+  }
+}
